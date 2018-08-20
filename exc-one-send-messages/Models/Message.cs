2ebd90a7c5415b50b332b9522c4200104baf843e
Grzegorz Oronowicz <goronowicz@pgs-soft.com>
2018-08-20 22:54:48 +0200

    Message model

---
+++ exc-one-send-messages/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exc_one_send_messages.Models
+{
+    public class Message
+    {
+        public Guid ID { get; set; }
+
+        public string Receipient { get; set; }
+
+        public string Content { get; set; }
+    }
+}
