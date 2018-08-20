c5a57a3f92f0a0d5f69fa5f62513549555a6dc6e
Grzegorz Oronowicz <goronowicz@pgs-soft.com>
2018-08-20 22:41:32 +0200

    Początek - ping

---
+++ exc-one-send-messages/Controllers/PublicController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace exc_one_send_messages.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublicController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        [Route("ping")]
+        public ActionResult<string> Get()
+        {
+            return "OK";
+        }
+    }
+}
