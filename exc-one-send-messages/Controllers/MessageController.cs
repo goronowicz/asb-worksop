7c530094fb2232ebda449191273d84ce2cd784f2
Grzegorz Oronowicz <goronowicz@pgs-soft.com>
2018-08-22 18:49:27 +0200

    QueueClient

--- exc-one-send-messages/Controllers/MessageController.cs
+++ exc-one-send-messages/Controllers/MessageController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using exc_one_send_messages.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
 
 namespace exc_one_send_messages.Controllers
 {
@@ -12,9 +15,15 @@ namespace exc_one_send_messages.Controllers
     [ApiController]
     public class MessageController : ControllerBase
     {
-        public async Task<ActionResult> Post([FromBody]Message message)
-        {
+        private readonly IQueueClient queueClient;
 
+        public MessageController(IQueueClient queueClient)
+        {
+            this.queueClient = queueClient;
+        }
+        public async Task<ActionResult> Post([FromBody]Models.Message message)
+        {
+            
             return Ok();
         }
     }
--- exc-one-send-messages/Startup.cs
+++ exc-one-send-messages/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +26,8 @@ namespace exc_one_send_messages
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IQueueClient, QueueClient>(s => new QueueClient(Configuration["serviceBusConnection"], "q-exc-one"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
